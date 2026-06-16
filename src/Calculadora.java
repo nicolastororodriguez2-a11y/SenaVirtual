0a342dc5a0e58ba8d983cffaa7fc6a89a2748de1
Nicolas toro <nicolastororodriguez2@gmail.com>
2026-06-16 18:20:51 -0500

    primera version

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
 
 public class Calculadora {
-    public int sumar(int num1, int num2){
+
+   public int sumar(int num1, int num2){
         return num1+num2;
     }
     public int restar(int num1, int num2){
