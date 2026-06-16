6b67814305515e935bb9b3ac00d48cd94c4cd9e0
Nicolas toro <nicolastororodriguez2@gmail.com>
2026-06-16 18:09:41 -0500

    primera version

---
+++ src/Calculadora.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Calculadora {
+    public int sumar(int num1, int num2){
+        return num1+num2;
+    }
+    public int restar(int num1, int num2){
+        return  num1-num2;
+    }
+    public int multiplicar(int num1, int num2){
+        return  num1*num2;
+    }
+    public double dividir(double num1, double num2){
+        return  num1/num2;
+    }
+
+    static void main() {
+        Calculadora calculadora =new Calculadora();
+        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor del numero 1: "));
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor del numero 2: "));
+        //Muestra el resultado en consola
+        System.out.println("El resultado de la suma es "+calculadora.sumar(num1,num2));
+        //Muestra el resultado en pantalla
+        JOptionPane.showMessageDialog(null,"El resultado de la suma es "+calculadora.sumar(num1,num2));
+    }
+    }
